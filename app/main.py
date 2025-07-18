834b90f760a98419adb9b9c9e24a001afab67130
Christopher Tonn <christopher.tonn@gmail.com>
2025-07-18 12:03:19 +0200

    • ADD a print statemant to test the data.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.routes import generate
+from app.song_title_data import song_title_data
 
 app = FastAPI()
 
 app.include_router(generate.router, prefix="/generate")
+
+print("🎧 Available Genres:\n")
+
+for key in song_title_data:
+    if key != "extended_mood_map":
+        print("-", key)
